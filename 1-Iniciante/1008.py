e96cfd0194d1743c44de4a92b2eeb110bd59c912
Cleiton <cleitonsilvatavares@gmail.com>
2019-10-26 09:37:00 -0300

    1008 - Salário

---
+++ 1-Iniciante/1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*
+
+numFuncionario = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+
+salario = horasTrabalhadas * valorHora
+
+print("NUMBER =" , numFuncionario)
+print("SALARY = U$ %.2f" % salario)
