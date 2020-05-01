bfeebdf32786182900a2c9031019b0d537c73bd2
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-05-01 18:14:11 -0300

    1078 - Tabuada

---
+++ 1-Iniciante/1078.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*
+
+# Variavel
+tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Entrada
+n = int(input())
+
+# Calcular tabuada
+for i in tabuada:
+    # Resultado
+    print(i, "x", n, "=", i * n)
