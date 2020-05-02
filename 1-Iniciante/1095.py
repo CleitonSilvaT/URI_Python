edb12729c33af95cc758f7cae2a2b25acb449b25
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-05-02 11:37:17 -0300

    1095 - Sequencia IJ 1

---
+++ 1-Iniciante/1095.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*
+
+# Variaveis
+j = 60
+i = 1
+
+# Encontrar todos os valores ate j = 0
+while ( j >= 0):
+    # Resposta
+    print("I=" + str(i), "J=" + str(j))
+    # Sequencia i aumenta em 3
+    i = i + 3
+    # Sequencia j diminui em 5
+    j = j - 5
