ca76ccbabb35a91bf0dc40398fd1f0e3e76bcf26
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-05-02 11:44:26 -0300

    1096 - Sequencia IJ 2

---
+++ 1-Iniciante/1096.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*
+
+# Variaveis
+i = 1
+j = [7, 6, 5]
+
+# Imprimir elemento de i até 9
+while (i <= 9):
+    # Imprimir valores pre-definidos de J
+    for num in j:
+        # Resposta
+        print("I=" + str(i), "J=" + str(num))
+    # Crecimento do i
+    i = i + 2
