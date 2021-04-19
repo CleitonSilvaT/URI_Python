860b5a1a22617be697a0b30e494fa6e136f2bb19
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2021-04-19 06:22:08 -0300

    1864 - Nossos Dias Nunca Voltarão

---
+++ 1-Iniciante/1864.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # Constante
+    citacao = 'LIFE IS NOT A PROBLEM TO BE SOLVED'
+
+    # Entrada
+    entrada = int(input())
+
+    # Imprimindo resultado
+    for i in range(entrada):
+        print(citacao[i], end='')
+    # Fim de linha
+    print()
