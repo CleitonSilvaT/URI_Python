20980563414205d3b4951a370ddb794b4c33ffb7
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2021-04-20 06:06:55 -0300

    1924 - Vitória e a Indecisão

---
+++ 1-Iniciante/1924.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Independente a lista de cursos informados
+# a resposta sempre sera Ciencia da Computacao
+if __name__ == '__main__':
+    # Entrada
+    casos_teste = int(input())
+
+    while(casos_teste > 0):
+        # Entrada
+        curso = input()
+        casos_teste -= 1
+    # Resultado
+    print('Ciencia da Computacao')
