53b39e426fef9f4ee9085ccb1f38fc7fdc873534
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2021-04-28 06:07:41 -0300

    1959 - Polígonos Regulares Simples

---
+++ 1-Iniciante/1959.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # Entrada
+    entrada = input()
+    valores = entrada.split(' ')
+
+    # Calculando perimetro
+    perimetro = int(valores[0]) * int(valores[1])
+
+    # Resultado
+    print(perimetro)
