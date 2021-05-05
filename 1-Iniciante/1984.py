6ffe44aab1f45d7fbf4b76c6badf23539826b376
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2021-05-05 06:37:42 -0300

    1984 - O Enigma do Pronalândia

---
+++ 1-Iniciante/1984.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # Entrada deve ser obrigatoriamente int
+    numero = int(input())
+    # Conversao para poder computar ordem inversa
+    unorder_number = str(numero)
+    # Resultado
+    print(unorder_number[::-1])
