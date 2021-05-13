4932d53160951c3da8b14a7a67db5cc330428b8d
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2021-05-13 06:03:00 -0300

    2160 - Nome no Formulário

---
+++ 1-Iniciante/2160.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    # Entrada
+    texto = input()
+    # Resultado
+    if(len(texto) > 80):
+        print('NO')
+    else:
+        print('YES')
